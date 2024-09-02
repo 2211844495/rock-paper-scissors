0719766ff5a8b1cfe9bd0210088d1776f1944eed
alaaahmed149 <anewalaahehe@gmail.com>
2024-09-02 23:41:19 +0200

    new Ui for the website

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-let humanScore = 0;
-let computerScore = 0;
+const humanScoreElement = document.getElementById("humanScore");
+const computerScoreElement = document.getElementById("computerScore");
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    console.log(playRound(getHumanChoice(), getComputerChoice(choices)));
-  }
-}
+let humanScore = parseInt(humanScoreElement.innerText, 10) || 0;
+let computerScore = parseInt(computerScoreElement.innerText, 10) || 0;
 
 function playRound(userChoice, computerChoice) {
   var message;
@@ -41,27 +38,48 @@ function playRound(userChoice, computerChoice) {
       humanScore++;
     }
   }
+
+  humanScoreElement.innerText = humanScore;
+  computerScoreElement.innerText = computerScore;
   return message;
 }
 
 const choices = ["rock", "paper", "scissors"];
-
+let computerC = document.getElementById("computerChoice");
+let humanSelection = "";
 function getComputerChoice(choices) {
-  return choices[Math.floor(Math.random() * choices.length)];
-}
-
-function getHumanChoice() {
-  let choice = prompt("Your Selection: ").toLowerCase();
+  let choice = choices[Math.floor(Math.random() * choices.length)];
+  if (choice === "rock") {
+    computerC.innerHTML = rock.innerHTML;
+    computerC.style = rock.style;
+  } else if (choice === "paper") {
+    computerC.innerHTML = paper.innerHTML;
+    computerC.style = paper.style;
+  } else {
+    computerC.innerHTML = scissors.innerHTML;
+    computerC.style = scissors.style;
+  }
   return choice;
 }
 
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+rock.addEventListener("mousedown", () => setHumanSelection("rock"));
+paper.addEventListener("mousedown", () => setHumanSelection("paper"));
+scissors.addEventListener("mousedown", () => setHumanSelection("scissors"));
+
+function setHumanSelection(selection) {
+  humanSelection = selection;
+  document.getElementById("result").innerText = playRound(humanSelection, getComputerChoice(choices));
+}
 
 
-playGame();
-if (humanScore > computerScore) {
+/* if (humanScore > computerScore) {
     console.log("Congrats :)! you won with a " + humanScore + " score!");
 } else if (humanScore < computerScore) {
     console.log("Sorry :(! you lost with a " + humanScore + " score.");
 } else {
     console.log("You did good! but it's a round game :).");
-}
+}*/
